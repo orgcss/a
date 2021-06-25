373a8718f9203ddafc7cbebdf37af3f3b41b7433
chenssgit <chenshangsong@hotmail.com>
2021-06-25 13:39:37 +0800

    Update xubuntu_init.sh

--- xubuntu_init.sh
+++ xubuntu_init.sh
@@ -2,3 +2,4 @@ sudo apt update
 sudo apt install -y fcitx-table-wbpy xfce4-genmon-plugin apache2 samba openssh shadowsocks privoxy 
 
 # /etc/fstab: auto,user,rw 1 2
+# .config/xfce4/terminal/ : geometry=80x24+100+100
